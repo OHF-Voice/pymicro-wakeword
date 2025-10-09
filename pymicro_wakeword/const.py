6df2f22de71240485beec94fe44d57bcfe4a933e
Michael Hansen <mike@rhasspy.org>
2025-10-09 11:11:34 -0500

    Clean up

--- pymicro_wakeword/const.py
+++ pymicro_wakeword/const.py
@@ -10,17 +10,3 @@ class Model(str, Enum):
     HEY_JARVIS = "hey_jarvis"
     HEY_MYCROFT = "hey_mycroft"
     ALEXA = "alexa"
-
-
-@dataclass
-class ClipResult:
-    """Result of process_clip."""
-
-    detected: bool
-    """True if wake word was detected in clip."""
-
-    detected_seconds: Optional[float] = None
-    """Seconds into the audio that wake word was detected."""
-
-    probabilities: Optional[List[float]] = None
-    """Probabilities for each feature window."""
