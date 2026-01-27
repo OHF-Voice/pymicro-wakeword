d9e4e9c6527f9ab91d18e6c77522c2dbf21dc7a1
Michael Hansen <mike@rhasspy.org>
2026-01-27 14:14:05 -0600

    Detect model stride

--- pymicro_wakeword/__main__.py
+++ pymicro_wakeword/__main__.py
@@ -1,27 +1,38 @@
 """Command-line utility for microWakeWord."""
 
 import argparse
+import logging
 import sys
 import wave
 
 from .const import Model
 from .microwakeword import MicroWakeWord, MicroWakeWordFeatures
 
+_LOGGER = logging.getLogger(__name__)
+
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", choices=[m.value for m in Model])
     parser.add_argument("--config", help="Path to JSON config")
+    parser.add_argument(
+        "--debug", action="store_true", help="Print debug message to console"
+    )
     parser.add_argument("wav_file", nargs="*")
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
+    _LOGGER.debug(args)
 
     if not (args.model or args.config):
         print("--model or --config is required", file=sys.stderr)
         sys.exit(1)
 
     if args.config:
+        _LOGGER.debug("Loading config from: %s", args.config)
         mww = MicroWakeWord.from_config(args.config)
     else:
+        _LOGGER.debug("Loading builtin model: %s", args.model)
         mww = MicroWakeWord.from_builtin(Model(args.model))
 
     mww_features = MicroWakeWordFeatures()
@@ -56,7 +67,7 @@ def main() -> None:
 
                 for features in mww_features.process_streaming(chunk):
                     if mww.process_streaming(features):
-                        print(args.model, flush=True)
+                        print(mww.wake_word, flush=True)
 
         except KeyboardInterrupt:
             pass
